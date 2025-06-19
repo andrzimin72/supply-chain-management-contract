7c35089aabb881cfcfd2e9dd8a4eea58d2fb3a58
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2025-06-19 10:01:13 +0800

    Delete Call_Contract_Function.jsx

--- Call_Contract_Function.jsx
+++
@@ -1,7 +0,0 @@
-const registerProducer = async () => {
-  if (contract) {
-    const tx = await contract.registerProducer("Farmer Joe");
-    await tx.wait();
-    alert("Producer registered!");
-  }
-};
